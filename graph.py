96250cfd617de5425bddc6bacf405dcd15eaf6d7
송지민 <sjm031009@gmail.com>
2026-06-15 20:02:33 +0900

    #1

--- graph.py
+++ graph.py
@@ -1,9 +1,11 @@
 from urllib.parse import parse_qs
-from template import html
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
+from template import html
 
 def application(environ, start_response):
-    if environ['PATH_INFO'] == '/graph.png':
+    if environ['PATH_INFO'] == '/img/graph.png':
         try:
             with open('./img/graph.png', 'rb') as f:
                 response_body = f.read()
